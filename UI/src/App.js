0def83b6857fd2f2a10ed6700cb5a1db7888a57f
Ashton Christensen <armadillomaster@gmail.com>
2019-03-28 19:58:56 -0600

    / and /auth/signup
    Better conditional rendering for root('/') and functionality to /auth/signup... I dont actually know if it works, becuase CORS said screw you. Hop[efully nginx will solve this problem when I implement it.

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from './components/header/';
 import Footer from './components/footer';
 import Login from './components/content/auth/login';
 import Signup from './components/content/auth/signup';
+import Bug from './components/content/report/bug';
+import Feature from './components/content/report/feature';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    state = {
-      userLoggedIn: null,
+    this.state = {
+      userLoggedIn: null
     };
   }
   render() {
@@ -20,33 +23,75 @@ class App extends Component {
           <Header />
           <Route
             exact
-            user={this.state.userLoggedIn}
             path="/"
-            component={Login}
+            render={props => (
+              <div className="root">
+                {this.state.userLoggedIn ? (
+                  <div className="isLoggedIn">
+                    <h2>
+                      Welcome to back to KTracker! You can now make a report:
+                    </h2>
+                    <br />
+                    <h3>
+                      <Link className="NavLink" to="/report/bug">
+                        <p>Bug</p>
+                      </Link>
+                      <br />
+                      OR
+                      <br />
+                      <Link className="NavLink" to="/report/feature">
+                        <p>Feature</p>
+                      </Link>
+                    </h3>
+                  </div>
+                ) : (
+                  <div className="notLoggedIn">
+                    <h2>
+                      Welcome to KTracker! Please login or signup to get
+                      started.
+                    </h2>
+                    <br />
+                    <h3>
+                      <Link className="NavLink" to="/auth/signup">
+                        <p>Signup</p>
+                      </Link>
+                      <br />
+                      OR
+                      <br />
+                      <Link className="NavLink" to="/auth/login">
+                        <p>Login</p>
+                      </Link>
+                    </h3>
+                  </div>
+                )}
+              </div>
+            )}
           />
           <Route
             exact
-            user={this.state.userLoggedIn}
             path="/auth/signup"
-            component={Signup}
+            render={props => (
+              <Signup {...props} user={this.state.userLoggedIn} />
+            )}
           />
           <Route
             exact
-            user={this.state.userLoggedIn}
             path="/auth/login"
-            component={Login}
+            render={props => (
+              <Login {...props} user={this.state.userLoggedIn} />
+            )}
           />
           <Route
             exact
-            user={this.state.userLoggedIn}
             path="/report/bug"
-            component={Login}
+            render={props => <Bug {...props} user={this.state.userLoggedIn} />}
           />
           <Route
             exact
-            user={this.state.userLoggedIn}
             path="/report/feature"
-            component={Login}
+            render={props => (
+              <Feature {...props} user={this.state.userLoggedIn} />
+            )}
           />
           <Footer />
         </div>
--- UI/src/components/content/auth/signup/index.js
+++ UI/src/components/content/auth/signup/index.js
@@ -1,52 +1,146 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './styles.css';
 
 class Signup extends Component {
-  handleSubmit = () => {
-    fetch('');
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+      username: '',
+      password: '',
+      agreeToTerms: false,
+      readyToSubmit: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setRedirect = this.setRedirect.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
   };
+
+  handleFormChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+    if (this.state.agreeToTerms && this.state.username && this.state.password) {
+      this.setState({ readyToSubmit: true });
+    }
+  };
+
+  signUp = (username, password) => {
+    let url = 'http://kraktracker.xyz/api/auth/signup';
+    let data = { username: username, password: password };
+    console.log(data);
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        console.log(json);
+      });
+  };
+
+  handleSubmit = async event => {
+    try {
+      await this.signUp(this.state.username, this.state.password);
+      this.props.history.push('/');
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+  componentDidUpdate() {
+    if (!this.state.readyToSubmit) {
+      if (
+        this.state.agreeToTerms &&
+        this.state.username &&
+        this.state.password
+      ) {
+        this.setState({ readyToSubmit: true });
+      }
+    }
+  }
   render() {
     let userLoggedIn = this.props.user;
     return (
       <div className="Signup">
-        <h2>
-          Signup
-          <span id="h2" />
-        </h2>
-
+        <h2>Signup</h2>
         <br />
-        <form>
-          <span>
-            <input
-              type="text"
-              required
-              autoComplete="true"
-              minLength={6}
-              placeholder="Username"
-              name="username"
-            />
-          </span>
-          <br />
-          <span>
-            <input
-              type="password"
-              required
-              autoComplete="true"
-              minLength={6}
-              placeholder="Password"
-              name="password"
-            />
-          </span>
-          <br />
-          <span>
-            <input type="checkbox" name="agreetoterms" />I agree to the terms of
-            service.
-          </span>
-          <br />
-          <button onClick={this.handleSubmit} type="submit" name="submitSignup">
-            Signup!
-          </button>
-        </form>
+        {userLoggedIn ? (
+          <p className="useralreadyloggedin">You are already logged in!</p>
+        ) : (
+          <form>
+            <span>
+              <input
+                type="text"
+                required
+                autoComplete="true"
+                minLength={6}
+                placeholder="Username"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleFormChange}
+              />
+            </span>
+            <br />
+            <span>
+              <input
+                type="password"
+                required
+                autoComplete="true"
+                minLength={6}
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleFormChange}
+              />
+            </span>
+            <br />
+            <span>
+              <input
+                type="checkbox"
+                name="agreeToTerms"
+                required
+                value={this.state.agreeToTerms}
+                onChange={this.handleFormChange}
+              />
+              I agree to the terms of service.
+            </span>
+            <br />
+            {this.state.readyToSubmit ? (
+              <button
+                onClick={() =>
+                  this.handleSubmit(this.state.username, this.state.password)
+                }
+                type="submit"
+                name="submitSignup"
+              >
+                Signup!
+              </button>
+            ) : (
+              <button
+                onClick={this.handleSubmit}
+                type="submit"
+                disabled="disabled"
+                name="submitSignup"
+              >
+                Signup!
+              </button>
+            )}
+          </form>
+        )}
       </div>
     );
   }
