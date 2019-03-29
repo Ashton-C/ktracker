8bb7519b58543c87023a77bfdac0a37442895413
Ashton Christensen <armadillomaster@gmail.com>
2019-03-28 21:49:09 -0600

    Basic form structure to bug and feature
    Just made the basic form structure for the /report components. Will come back for actual functionality and CSS tomorrow.

--- UI/src/components/content/report/bug/index.js
+++ UI/src/components/content/report/bug/index.js
@@ -2,6 +2,34 @@ import React, { Component } from 'react';
 
 export default class Bug extends Component {
   render() {
-    return <div className="Bug" />;
+    return (
+      <div className="Bug">
+        <h2>Bug Report</h2>
+        <form>
+          <input type="text" name="name" placeholder="Bug Name" />
+          <br />
+          <input type="text" name="description" placeholder="Description" />
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="platform"
+              value="UI (Web)"
+              placeholder="UI (Web)"
+            />
+            UI (Web)
+          </label>
+          <br />
+          <label>
+            <input type="radio" name="platform" value="Distro (Windows .exe)" />
+            Distro (Windows .exe)
+          </label>
+          <br />
+          <button type="submit" name="submit" onClick={this.handleSubmit}>
+            Submit!
+          </button>
+        </form>
+      </div>
+    );
   }
 }
